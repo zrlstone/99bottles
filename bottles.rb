7b04502b9fbd01d9ececcc18118471947c1125f9
zrlstone <zak.stone@materialsmarket.com>
2023-04-22 08:54:52 +0100

    adds BottleNumber1

--- bottles.rb
+++ bottles.rb
@@ -18,8 +18,11 @@ class Bottles
   end
 
   def bottle_number_for(number)
-    if number == 0
+    case number
+    when 0
       BottleNumber0
+    when 1
+      BottleNumber1
     else
       BottleNumber
     end.new(number)
@@ -36,35 +39,19 @@ class BottleNumber
   end
 
   def successor
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    number - 1
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    "bottles"
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def quantity
@@ -75,5 +62,23 @@ end
 class BottleNumber0 < BottleNumber
   def quantity
     "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    99
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
   end
 end
